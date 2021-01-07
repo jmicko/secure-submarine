5685de228fbaa842bb0d83818a93ea3bd538dd66
John Micko <jrmicko5012@gmail.com>
2021-01-06 20:38:32 -0600

    users can only access permissible secrets

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
+  let level = req.user.clearance_level;
+  let querytext = `SELECT * FROM "secret" WHERE "secrecy_level" < $1;`;
   pool
-    .query('SELECT * FROM "secret";')
+    .query(querytext, [level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
